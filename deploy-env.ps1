c37b882a44d4238dbe81aab16383b1498010aa36
Robbin Verbist <robbin@verbistenverbist.be>
2026-03-28 13:38:30 +0100

    fix: correct service name in compose file and update deployment target path

--- deploy-env.ps1
+++ deploy-env.ps1
@@ -1,4 +1,6 @@
-$target = "rverbist@proxmox:/opt/docker/apps/UnifiAssistant"
+$target = "rverbist@proxmox:/opt/docker/apps/UnifiStoreWatcher"
 
 scp ".env.Production" "$target/.env.Production"
 scp ".env" "$target/.env"
+scp "compose.yaml" "$target/compose.yaml"
+scp "Dockerfile" "$target/Dockerfile"
